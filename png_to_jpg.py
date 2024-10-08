d83df965b1966aa0a4ad0ba6e8ab1f0fabd8aa3c
Neo Yin <yinzhang@berkeley.edu>
2024-10-07 21:12:31 -0700

    random numerical grad experiments

---
+++ png_to_jpg.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+from tqdm import tqdm
+
+png_dir = "/media/hdd3/neo/not_enough_focus_regions_topviews"
+save_dir = "/media/hdd3/neo/not_enough_focus_regions_topviews_jpg"
+
+os.makedirs(save_dir, exist_ok=True)
+# get the paths to all the png files in the directory
+png_files = [os.path.join(png_dir, f) for f in os.listdir(png_dir) if f.endswith(".png")]
+
+
+for png_file in tqdm(png_files, desc="Converting PNG to JPG"):
+    # Open the PNG file
+    img = Image.open(png_file)
+    
+    # Save the image as a JPG file
+    jpg_path = os.path.join(save_dir, os.path.basename(png_file).replace(".png", ".jpg"))
